3e6dfb76326188ff730b3bfdd2ce61e398067602
Guan_MingHao <1071969980@qq.com>
2025-06-16 20:19:59 +0800

    docker 构建和部署

---
+++ pip_resources/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/explosion/spacy-models/releases/download/zh_core_web_trf-3.8.0/zh_core_web_trf-3.8.0-py3-none-any.whl
+wget https://github.com/explosion/spacy-models/releases/download/zh_core_web_sm-3.8.0/zh_core_web_sm-3.8.0-py3-none-any.whl
