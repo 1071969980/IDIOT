65740e3d3de48cd36aa496c04b970674c1839094
Guan MingHao <1071969980@qq.com>
2025-07-02 14:50:48 +0800

    fix(docker): 修正 seaweedfs 数据目录路径
    - 移除了 ./volumes/file_cache/* 的清理操作
    - 将 ./volumes/seaweed/data/* 修改为 ./volumes/seaweedfs/data/*

--- docker/clean_volumes.sh
+++ docker/clean_volumes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -rf ./volumes/cortex/*
-rm -f ./volumes/file_cache/*
 rm -f ./volumes/jaeger/*
 rm -rf ./volumes/prometheus/*
-rm -rf ./volumes/seaweed/data/*
+rm -rf ./volumes/seaweedfs/data/*
