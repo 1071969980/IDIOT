e014cb8776c20c94ba358f0f25f3ba55f0739e1d
Guan MingHao <1071969980@qq.com>
2025-07-03 15:28:55 +0800

    refactor(load_balance): 重构负载均衡模块
    - 导入新的 init 模块以初始化负载均衡器
    - 更新导入内容，改为从 constant 模块导入常量

--- api/load_balance/__init__.py
+++ api/load_balance/__init__.py
@@ -1,33 +1,6 @@
-from api.llm.deepseek import async_client as deepseek_async_client
-from api.llm.qwen import async_client as qwen_async_client
+from .init import *
 
-from .load_balancer import LoadBalancer
-from .service_instance import AsyncOpenAIServiceInstance
-from .service_regeistry import ServiceConfig, ServiceRegistry
-
-LOAD_BLANCER = LoadBalancer(ServiceRegistry())
-
-DEEPSEEK_REASONER_SERVICE_NAME = "deepseek"
-
-def register_deepseek_service() -> None:
-    service_reg = LOAD_BLANCER.registry
-    # tongyi service for deepseek
-    tongyi_instance = AsyncOpenAIServiceInstance(
-        name="tongyi",
-        openai_client=qwen_async_client(),
-        model="deepseek-r1-0528",
-    )
-    service_reg.register_service(DEEPSEEK_REASONER_SERVICE_NAME,
-                                 tongyi_instance)
-
-    # deepseek offical service
-    deepseek_offcial_instance = AsyncOpenAIServiceInstance(
-        name="deepseek",
-        openai_client=deepseek_async_client(),
-        model="deepseek-reasoner",
-    )
-    service_reg.register_service(DEEPSEEK_REASONER_SERVICE_NAME,
-                                 deepseek_offcial_instance)
-    
-register_deepseek_service()
-
+from constant import (
+    LOAD_BLANCER,
+    DEEPSEEK_REASONER_SERVICE_NAME,
+)
