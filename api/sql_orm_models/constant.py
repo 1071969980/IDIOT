144a01ddffb08ff86a001207f1c8d341431d334f
Guan MingHao <1071969980@qq.com>
2025-10-09 22:21:49 +0800

    feat(sql): 添加对 asyncpg 异步数据库驱动的支持
    新增 asyncpg 依赖以支持 SQLAlchemy 的异步数据库操作，并配置了异步数据库连接 URL。同时更新了相关依赖文件，包括 pyproject.toml、requirements.txt 和 uv.lock，确保项目依赖一致且支持异步数据库引擎。

--- api/sql_orm_models/constant.py
+++ api/sql_orm_models/constant.py
@@ -14,7 +14,15 @@ sql_url  = URL.create(
     port=5432,
     database=str(DEFAULT_DATA_BASE_NAME),
 )
+async_sql_url  = URL.create(
+    drivername="postgresql+asyncpg",
+    username="postgres",
+    password="postgres",
+    host="postgres",
+    port=5432,
+    database=str(DEFAULT_DATA_BASE_NAME),
+)
 
 SQL_ENGINE = create_engine(sql_url)
-ASYNC_SQL_ENGINE = create_async_engine(sql_url, future=True)
+ASYNC_SQL_ENGINE = create_async_engine(async_sql_url, future=True)
 DEFAULT_SQL_ENGINE_POOL = SQL_ENGINE.pool
